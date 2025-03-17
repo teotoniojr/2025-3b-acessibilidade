7ca7cfecd8e4582b4f0a42de8479300a0dc7de52
Teotonio José de Souza Junior <102707827+teotoniojr@users.noreply.github.com>
2025-03-17 08:41:21 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
+
 const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+const diminuirFonteBotao = document.getElementById('diminuir-fonte')
 
 let tamanhoFonteAtual = 1;
 
 aumentaFonteBotao.addEventListener('click', function() {
     tamanhoFonteAtual += 0.1;
+    document.body.style.fontSize = `${tamanhoFonteAtual}rem`
+})
+
+diminuirFonteBotao.addEventListener('click', function() {
+    tamanhoFonteAtual -= 0.1;
     document.body.style.fontSize = `${tamanhoFonteAtual}rem`
 })

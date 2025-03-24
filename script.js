4b972ae1e9ddc59867e6df8fe7abbfe88c5c2e01
Teotonio José de Souza Junior <102707827+teotoniojr@users.noreply.github.com>
2025-03-24 08:23:21 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const botaoAcessibilidade = document.getElementById('botao-acessibilidade');
+    const opcoesAcessibilidade = document.getElementById('apresenta-lista');
 
-const aumentaFonteBotao = document.getElementById('aumentar-fonte');
-const diminuirFonteBotao = document.getElementById('diminuir-fonte')
+    botaoAcessibilidade.addEventListener('click', function(){
+        botaoAcessibilidade.classList.toggle('rotacao-menu');
+        opcoesAcessibilidade.classList.toggle('apresenta-lista');
+    })
 
-let tamanhoFonteAtual = 1;
 
-aumentaFonteBotao.addEventListener('click', function() {
-    tamanhoFonteAtual += 0.1;
-    document.body.style.fontSize = `${tamanhoFonteAtual}rem`
-})
+    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+    const diminuirFonteBotao = document.getElementById('diminuir-fonte')
 
-diminuirFonteBotao.addEventListener('click', function() {
-    tamanhoFonteAtual -= 0.1;
-    document.body.style.fontSize = `${tamanhoFonteAtual}rem`
-})
+    let tamanhoFonteAtual = 1;
+
+    aumentaFonteBotao.addEventListener('click', function () {
+        tamanhoFonteAtual += 0.1;
+        document.body.style.fontSize = `${tamanhoFonteAtual}rem`
+    })
+
+    diminuirFonteBotao.addEventListener('click', function () {
+        tamanhoFonteAtual -= 0.1;
+        document.body.style.fontSize = `${tamanhoFonteAtual}rem`
+    })
+});
